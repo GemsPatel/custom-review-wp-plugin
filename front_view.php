ddffa303fb5ee83c58b9ce8f049f50146dcb8c9a
DESKTOP-LHTD35A\Hp <kakdiya.gautam288@gmail.com>
2022-05-17 12:58:16 -0700

    remove unneccessory loop

--- front_view.php
+++ front_view.php
@@ -1,63 +1,56 @@
 <?php
+$dbTable = [
+	'rvcomment' => $wpdb->prefix.'rvcomment', //table_name1
+	'services' => $wpdb->prefix.'services', //table_name3
+	'client' => $wpdb->prefix.'client', //table_client
+];
+
+$where = "";
+if (array_key_exists("category",$rv_view_data)){
+	if( $rv_view_data['category'] == 0 ){
+		$rowClt23 = $wpdb->get_results("select * from ".$dbTable['rvcomment']." where review_status=1 AND act=1", ARRAY_A);	
+		$total_c = $wpdb->num_rows;	
+		$rowClt = $wpdb->get_results("select * from ".$dbTable['rvcomment']." where review_status=1 AND act=1 order by id DESC LIMIT 10", ARRAY_A);
+	} else{
+		$rowClt23 = $wpdb->get_results("select * from ".$dbTable['rvcomment']." where review_status=1 AND act=1 AND service_id='".$rv_view_data['category']."'", ARRAY_A);
+		$total_c = $wpdb->num_rows;
+		$where = "AND service_id='".$rv_view_data['category']."'";
+		$rowClt = $wpdb->get_results("select * from ".$dbTable['rvcomment']." where review_status=1 AND act=1 AND service_id='".$rv_view_data['category']."' order by id DESC LIMIT 10", ARRAY_A);
+		$total_c = $wpdb->num_rows;
+		$rowSrv = $wpdb->get_row("select * from ".$dbTable['services']." where id='".$rv_view_data['category']."' ", ARRAY_A);
+		$SrvName = $rowSrv['name'];
+	}
+} else if (array_key_exists("clientid",$rv_view_data)) {
+	$rowClt = $wpdb->get_results("select * from ".$dbTable['rvcomment']." where review_status=1 AND act=1 AND client_id='".$rv_view_data['clientid']."' order by id DESC ", ARRAY_A);
+	$total_c = $wpdb->num_rows;
+	$rowClient = $wpdb->get_row("select * from ".$dbTable['client']." where id='".$rv_view_data['clientid']."' ", ARRAY_A);
+	$where = "AND client_id='".$rv_view_data['clientid']."'";
+	$GetRow = $wpdb->get_row("select * from ".$dbTable['rvcomment']." where review_status=1 AND client_id='".$rv_view_data['clientid']."' AND act=1 order by id DESC LIMIT 1", ARRAY_A);
+	$rowSrv = $wpdb->get_row("select * from ".$dbTable['services']." where id='".$GetRow['service_id']."' ", ARRAY_A);
+	$SrvName = $rowSrv['name'];
+	$date = date_parse_from_format("Y-m-d h:i:s", $rowClient['datetime']);
+	$time = mktime($date['hour'], $date['minute'], $date['second'], $date['month'], $date['day'], $date['year']);
+	$add_date = date('M d, Y', $time);
+} else {
+	$rowClt = $wpdb->get_results("select * from ".$dbTable['rvcomment']." where review_status=1 AND act=1", ARRAY_A);
+	$total_c = $wpdb->num_rows;
+	$SrvName = 'Multiple Services';
+}
+
+$result = $wpdb->get_results("SELECT sum(review_rating) as total_rv FROM ".$dbTable['rvcomment']." where review_status=1 AND act=1 ".$where );
+$sum = $result[0]->total_rv;
+$average = 0;
+if($sum !='0' AND $total_c !='0') {
+	$average = $sum/$total_c;
+}
+
 $return_v ='
 <div class="" style=" padding-left:15px; padding-right:15px;">
 	<div class="row">
 		<div class="col-sm-12" style="padding:0;">
-			<div id="" class="owl-carousel owl-theme">'; ?>
-			<?php
-			$dbTable = [
-				'rvcomment' => $wpdb->prefix.'rvcomment', //table_name1
-				'services' => $wpdb->prefix.'services', //table_name3
-				'client' => $wpdb->prefix.'client', //table_client
-			];
-
-			$where = "";
-			if (array_key_exists("category",$rv_view_data)){
-				if( $rv_view_data['category'] == 0 ){
-					$rowClt23 = $wpdb->get_results("select * from ".$dbTable['rvcomment']." where review_status=1 AND act=1", ARRAY_A);	
-					$total_c = $wpdb->num_rows;	
-					$rowClt = $wpdb->get_results("select * from ".$dbTable['rvcomment']." where review_status=1 AND act=1 order by id DESC LIMIT 10", ARRAY_A);
-				} else{
-					$rowClt23 = $wpdb->get_results("select * from ".$dbTable['rvcomment']." where review_status=1 AND act=1 AND service_id='".$rv_view_data['category']."'", ARRAY_A);
-					$total_c = $wpdb->num_rows;
-					$where = "AND service_id='".$rv_view_data['category']."'";
-					$rowClt = $wpdb->get_results("select * from ".$dbTable['rvcomment']." where review_status=1 AND act=1 AND service_id='".$rv_view_data['category']."' order by id DESC LIMIT 10", ARRAY_A);
-					$total_c = $wpdb->num_rows;
-					$rowSrv = $wpdb->get_row("select * from ".$dbTable['services']." where id='".$rv_view_data['category']."' ", ARRAY_A);
-					$SrvName = $rowSrv['name'];
-				}
-			} else if (array_key_exists("clientid",$rv_view_data)) {
-				$rowClt = $wpdb->get_results("select * from ".$dbTable['rvcomment']." where review_status=1 AND act=1 AND client_id='".$rv_view_data['clientid']."' order by id DESC ", ARRAY_A);
-				$total_c = $wpdb->num_rows;
-				$rowClient = $wpdb->get_row("select * from ".$dbTable['client']." where id='".$rv_view_data['clientid']."' ", ARRAY_A);
-				$where = "AND client_id='".$rv_view_data['clientid']."'";
-				$GetRow = $wpdb->get_row("select * from ".$dbTable['rvcomment']." where review_status=1 AND client_id='".$rv_view_data['clientid']."' AND act=1 order by id DESC LIMIT 1", ARRAY_A);
-				$rowSrv = $wpdb->get_row("select * from ".$dbTable['services']." where id='".$GetRow['service_id']."' ", ARRAY_A);
-				$SrvName = $rowSrv['name'];
-				$date = date_parse_from_format("Y-m-d h:i:s", $rowClient['datetime']);
-				$time = mktime($date['hour'], $date['minute'], $date['second'], $date['month'], $date['day'], $date['year']);
-				$add_date = date('M d, Y', $time);
-				
-				$return_v .='';
-			} else {
-				$rowClt = $wpdb->get_results("select * from ".$dbTable['rvcomment']." where review_status=1 AND act=1", ARRAY_A);
-				$total_c = $wpdb->num_rows;
-				$SrvName = 'Multiple Services';
-			}
-
-			$result = $wpdb->get_results("SELECT sum(review_rating) as total_rv FROM ".$dbTable['rvcomment']." where review_status=1 AND act=1 ".$where );
-			$sum = $result[0]->total_rv;
-			$average = 0;
-			if($sum !='0' AND $total_c !='0') {
-				$average = $sum/$total_c;
-			}
-
-			$rr=0;
-			foreach($rowClt as $client) {
-				$rr++;
-				$rowClient = $wpdb->get_row("select * from ".$dbTable['client']." where id='".$client['client_id']."' ", ARRAY_A);
-		
-				$return_v .='
+			<div id="" class="owl-carousel owl-theme">'; 
+				foreach($rowClt as $client) {
+					$return_v .='
 					<div class="item">
 						<div class="media">
 							<div class="media-body">
@@ -88,9 +81,25 @@ $return_v ='
 								</div>
 							</div>
 						</div>';
-			}
-		$return_v .="</div>
-	</div>";
+				}
+			$return_v .="</div>
+		</div>";
+
+	if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
+         $url = "https://";   
+    else  
+         $url = "http://";   
+    // Append the host(domain name, ip) to the URL.   
+    $url.= $_SERVER['HTTP_HOST'];   
+    
+    // Append the requested resource location to the URL   
+    $url.= $_SERVER['REQUEST_URI']."?view=all";    
+
+	$return_v .='<div class"row">
+					<div class="col-md-12 text-center" style="margin-top: 15px;">
+						<a type="button" class="btn btn-primary" href="'.$url.'" target="_blank">Load All</a>
+					</div>
+				</div>';
 	$return_v .='
 	<script type="application/ld+json">[{"@context":"http:\/\/schema.org","@type":"Review","itemReviewed":{"@type":"LocalBusiness","name":"'.$rowClient['clientName'].'","url": "'.get_permalink(get_the_ID()).'",
 	"image": "'.site_url().'/wp-content/plugins/theme-options/img/guarantee-shield-big.png", "priceRange": "££" },"reviewRating":{"@type":"aggregateRating","ratingValue":'.round($average,2).',"bestRating":5,"reviewCount":'.$total_c.'},"author":"Users"}]</script>
--- review_form.php
+++ review_form.php
@@ -1,182 +1,178 @@
-<div style="
-	 padding-left:15px;
-	 padding-right:15px;
- ">
+<div style="padding-left:15px; padding-right:15px;">
 	<div class="row">
 		<div class="col-sm-12" style="padding:0;">
-		<?php
-		if(isset($_REQUEST['rv_name']))
-   {
-	global $wpdb;
-	file_put_contents('sheet.txt', $_REQUEST['rv_name']);
-	$rv_name = $_REQUEST['rv_name'];
-	$client_id = $_REQUEST['rv_clientid'];
-	$rv_category = $_REQUEST['rv_category'];
-	$rv_phone = $_REQUEST['rv_phone'];
-	$rv_email = $_REQUEST['rv_email'];
-	$rv_gender = $_REQUEST['rv_gender'];
-	$rv_comment = $_REQUEST['rv_comment'];
-	$review_rating = $_REQUEST['review_rating'];
-	$rv_post_id = $_REQUEST['rv_post_id'];
-	$date_time = date('Y-m-d h:i:s');
-	$wp_client = $wpdb->prefix.'client';
-	$num = $wpdb->query("select * from $wp_client where id='$client_id' ");
-	if($wpdb->num_rows > 0)
-	{
-		$row = $wpdb->get_row($wpdb->prepare( "SELECT * FROM $wp_client WHERE id='$client_id' " ), ARRAY_A );
-		$rv_category =  $row['serviceType'];
-		$to_email =  $row['email'];
-		$wp_rvcomment = $wpdb->prefix.'rvcomment';
-		$wpdb->query("select * from $wp_rvcomment where reviewer_email='$rv_email' AND client_id='$client_id'AND act='1' "); 
-		if($wpdb->num_rows =='0')
-		{
-			$wpdb->query("INSERT INTO $wp_rvcomment SET date_time='$date_time' , reviewer_name='$rv_name' , reviewer_email='$rv_email' , review_title='$rv_name', review_rating='$review_rating', review_text='$rv_comment', client_id='$client_id', review_phone='$rv_phone', service_id='$rv_category', review_status='0', post_id='$rv_post_id'");
-			$html_content = 'Hi,<br>';
-			$html_content .= '<br>You just received a new review on My Trusted Expert. Pleases see below:';
-			$html_content .= '<br><br><b>Rating:</b> '.$review_rating;
-			$html_content .= '<br><b>Name:</b> '.$rv_name;
-			$html_content .= '<br><b>Review:</b> '.$rv_comment;
-			$html_content .= '<br><br>Regards,';
-			$html_content .= '<br><b>My Trusted Expert Team</b>';
-			$api_key='f5dd543999420f1e4816d50234f94826-e566273b-94e1f876';//paste the api key here
-			$app_url='https://api.mailgun.net/v3/sandbox64b28936992444bdadafd1c648aff050.mailgun.org';//paste here the sub domain as test.testing.com
-			$_mytrustedexpert_from_email = get_option('_mytrustedexpert_from_email');
-			if($_mytrustedexpert_from_email == ''){
-				$from_mail = get_option('admin_email');
-			}else{
-				$from_mail = $_mytrustedexpert_from_email;
-			}
-			$curl_post_data = [
-				'from' =>'Mytrustedexpert.com <'.$from_mail.'>',
-				'to' =>'User <'.$to_email.'>',
-				'subject' =>'New Review',
-				'replyTo' =>$from_mail,
-				'Message-Id' =>"mytrustedexpert_email",
-				'html' =>$html_content,
-				'o:tracking-clicks' =>False
-			];
-			$api_key = get_option('_mytrustedexpert_mailgun_api_key');
-			$app_url = get_option('_mytrustedexpert_mailgun_domain');
-			if($api_key != '' && $app_url != ''){
-				//$rev_mail = rev_mail($app_url,$api_key,$curl_post_data);
-			}
-			echo' <div id="rv_success" class="alert alert-success fade in alert-dismissable" style="margin-top:10px;">
-			<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">x</a>
-			<center>
-			<strong>
-			Your review has been recorded successfully and submitted for approval. Thanks!</strong>
-			</center>
-			</div>';
-			$sc ='1';
-		}
-		else{
-			echo'<div class="alert alert-danger fade in alert-dismissable" style="margin-top:18px;">
-			<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">x</a>
-			<center>
-			<strong>
-			Duplicate review not allowed.</strong>
-			</center></div>';
-		}
-    }
-   }
-		 if(isset($sc)) { ?>
-		<?php } else{ 
-			if($_GET['clientid'] != ''){
-				$clientid = $_GET['clientid'];
+			<?php
+			$sc = null;
+			if(isset($_REQUEST['rv_name']))
+			{
+				global $wpdb;
+				file_put_contents('sheet.txt', $_REQUEST['rv_name']);
+				$rv_name = $_REQUEST['rv_name'];
+				$client_id = $_REQUEST['rv_clientid'];
+				$rv_category = $_REQUEST['rv_category'];
+				$rv_phone = $_REQUEST['rv_phone'];
+				$rv_email = $_REQUEST['rv_email'];
+				$rv_gender = $_REQUEST['rv_gender'];
+				$rv_comment = $_REQUEST['rv_comment'];
+				$review_rating = $_REQUEST['review_rating'];
+				$rv_post_id = $_REQUEST['rv_post_id'];
+				$date_time = date('Y-m-d h:i:s');
 				$wp_client = $wpdb->prefix.'client';
-				$num = $wpdb->query("select * from $wp_client where id='$clientid' ");
+				$num = $wpdb->query("select * from $wp_client where id='$client_id' ");
 				if($wpdb->num_rows > 0)
 				{
-					$row = $wpdb->get_row($wpdb->prepare( "SELECT * FROM $wp_client WHERE id='$clientid' " ), ARRAY_A );
-					$category =  $row['serviceType'];
-				}else{
-					echo'<div class="alert alert-danger fade in alert-dismissable" style="margin-top:18px;">
-			<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">x</a>
-			<center>
-			<strong>
-			Client not found.</strong>
-			</center></div>';
-				}
-			}
-			?>
-			 <div class="box box-primary" id="rv_form_div">
-                <div class="box-header with-border">
-                  <h3 class="box-title">Leave your review</h3>  
-                </div><!-- /.box-header -->
-                <!-- form start -->
-                <form role="form" method="POST" action="" id="rv_review_form" >
-                  <div class="box-body">
-				  <?php
-				  if($_GET['clientid'] != ''){ 
-					if($clientid != '' && $category != ''){
-						echo "<input type='hidden' name='rv_clientid' value='".$clientid."'>";
-						echo "<input type='hidden' name='rv_category' value='".$category."'>";
+					$row = $wpdb->get_row($wpdb->prepare( "SELECT * FROM $wp_client WHERE id='$client_id' " ), ARRAY_A );
+					$rv_category =  $row['serviceType'];
+					$to_email =  $row['email'];
+					$wp_rvcomment = $wpdb->prefix.'rvcomment';
+					$wpdb->query("select * from $wp_rvcomment where reviewer_email='$rv_email' AND client_id='$client_id'AND act='1' "); 
+					if( $wpdb->num_rows == 0 )
+					{
+						$wpdb->query("INSERT INTO $wp_rvcomment SET date_time='$date_time' , reviewer_name='$rv_name' , reviewer_email='$rv_email' , review_title='$rv_name', review_rating='$review_rating', review_text='$rv_comment', client_id='$client_id', review_phone='$rv_phone', service_id='$rv_category', review_status='0', post_id='$rv_post_id'");
+						$html_content = 'Hi,<br>';
+						$html_content .= '<br>You just received a new review on My Trusted Expert. Pleases see below:';
+						$html_content .= '<br><br><b>Rating:</b> '.$review_rating;
+						$html_content .= '<br><b>Name:</b> '.$rv_name;
+						$html_content .= '<br><b>Review:</b> '.$rv_comment;
+						$html_content .= '<br><br>Regards,';
+						$html_content .= '<br><b>My Trusted Expert Team</b>';
+						$api_key='f5dd543999420f1e4816d50234f94826-e566273b-94e1f876';//paste the api key here
+						$app_url='https://api.mailgun.net/v3/sandbox64b28936992444bdadafd1c648aff050.mailgun.org';//paste here the sub domain as test.testing.com
+						$_mytrustedexpert_from_email = get_option('_mytrustedexpert_from_email');
+						if($_mytrustedexpert_from_email == ''){
+							$from_mail = get_option('admin_email');
+						}else{
+							$from_mail = $_mytrustedexpert_from_email;
+						}
+						$curl_post_data = [
+							'from' =>'Mytrustedexpert.com <'.$from_mail.'>',
+							'to' =>'User <'.$to_email.'>',
+							'subject' =>'New Review',
+							'replyTo' =>$from_mail,
+							'Message-Id' =>"mytrustedexpert_email",
+							'html' =>$html_content,
+							'o:tracking-clicks' =>False
+						];
+						$api_key = get_option('_mytrustedexpert_mailgun_api_key');
+						$app_url = get_option('_mytrustedexpert_mailgun_domain');
+						if($api_key != '' && $app_url != ''){
+							//$rev_mail = rev_mail($app_url,$api_key,$curl_post_data);
+						}
+						echo' <div id="rv_success" class="alert alert-success fade in alert-dismissable" style="margin-top:10px;">
+						<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">x</a>
+						<center>
+						<strong>
+						Your review has been recorded successfully and submitted for approval. Thanks!</strong>
+						</center>
+						</div>';
+						$sc = 1;
 					}
-				  }else{
-					if (array_key_exists("category",$rv_form_data))
-					  {
-					  echo "<input type='hidden' name='rv_category' value='".$rv_form_data['category']."'>";
-					  }
-					  if (array_key_exists("clientid",$rv_form_data))
-					  {
-					  echo "<input type='hidden' name='rv_clientid' value='".$rv_form_data['clientid']."'>";
-					  }
-					  else{
-						?>
-						<div class="form-group">
-							<select  name='rv_clientid' required class="form-control" >
-							<option value="" >Select Person</option>
-								<?php  $table_name1 = $wpdb->prefix.'client';
-								$rowClt = $wpdb->get_results("select * from $table_name1 where act='1' ", ARRAY_A);
-								foreach($rowClt as $client)
-								{
-								echo'<option value="'. $client['id'].'" >'. $client['clientName'].'</option>';
+					else{
+						echo'<div class="alert alert-danger fade in alert-dismissable" style="margin-top:18px;">
+						<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">x</a>
+						<center>
+						<strong>
+						Duplicate review not allowed.</strong>
+						</center></div>';
+					}
+				}
+   			}
+
+		 	if( $sc == null ) {
+				if($_GET['clientid'] != ''){
+					$clientid = $_GET['clientid'];
+					$wp_client = $wpdb->prefix.'client';
+					$num = $wpdb->query("select * from $wp_client where id='$clientid' ");
+					if($wpdb->num_rows > 0)
+					{
+						$row = $wpdb->get_row($wpdb->prepare( "SELECT * FROM $wp_client WHERE id='$clientid' " ), ARRAY_A );
+						$category =  $row['serviceType'];
+					}else{
+						echo'<div class="alert alert-danger fade in alert-dismissable" style="margin-top:18px;">
+						<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">x</a>
+						<center>
+							<strong>Client not found.</strong>
+						</center></div>';
+					}
+				} ?>
+			 	<div class="box box-primary" id="rv_form_div">
+                	<div class="box-header with-border">
+                  		<h3 class="box-title">Leave your review</h3>  
+                	</div><!-- /.box-header -->
+                
+					<!-- form start -->
+                	<form role="form" method="POST" action="" id="rv_review_form" >
+                  		<div class="box-body">
+				  			<?php
+				  			if($_GET['clientid'] != ''){ 
+								if($clientid != '' && $category != ''){
+									echo "<input type='hidden' name='rv_clientid' value='".$clientid."'>";
+									echo "<input type='hidden' name='rv_category' value='".$category."'>";
 								}
-								?>
-							</select>
-						</div>
-		            <?php } }?>
-                    <div class="form-group">
-                      <input type="text" name='rv_name' required class="form-control"  placeholder="Enter name *"> 
-					  <input type="hidden" name='rv_post_id' required value="<?php echo get_the_ID(); ?>" >
-                    </div>  
-                    <div class="form-group">
-                       <input type="text" name='rv_phone' class="form-control" required  placeholder="Enter telephone number (not displayed) *">
-                    </div>  
-					<div class="form-group">
-                       <input type="email" name='rv_email' class="form-control" required  placeholder="Enter e-mail (not displayed) *">
-                    </div>
-                   <div class="form-group"  style="display:none">
-                      <div class="radio">
-                        <label>
-                          <input type="radio"  name="rv_gender" id="optionsRadios1" value="Male" checked>
-                          Male
-                        </label>
-                      </div>
-                      <div class="radio">
-                        <label>
-                          <input type="radio" name="rv_gender" id="optionsRadios2" value="Female">
-                          Female
-                        </label>
-                      </div>
-                    </div>
-					<div class="form-group">
-                  <input type="text" name="review_rating" required class="kv-uni-star rating-loading" value="" data-size="lg" title="">
-        <br>
-                    </div>
-					<div class="form-group">
-                      <textarea class="form-control" rows="4" name="rv_comment" required placeholder="Leave your review...*"></textarea>
-                    </div>
-                  </div><!-- /.box-body -->
-                  <div class="box-footer">
-                    <button type="submit" name="rv_submit" class="btn btn-primary">Submit Review</button>
-                  </div>
-                </form>
-              </div><!-- /.box -->
-		<?php } ?>
-			</div>
+				  			}else{
+								if (array_key_exists("category",$rv_form_data)) {
+					  				echo "<input type='hidden' name='rv_category' value='".$rv_form_data['category']."'>";
+					  			}
+					  			
+								if (array_key_exists("clientid",$rv_form_data)){
+									echo "<input type='hidden' name='rv_clientid' value='".$rv_form_data['clientid']."'>";
+					  			}else{ ?>
+									<div class="form-group">
+										<select  name='rv_clientid' required class="form-control" >
+											<option value="" >Select Person</option>
+											<?php  $table_name1 = $wpdb->prefix.'client';
+											$rowClt = $wpdb->get_results("select * from $table_name1 where act='1' ", ARRAY_A);
+											foreach($rowClt as $client) {
+												echo'<option value="'. $client['id'].'" >'. $client['clientName'].'</option>';
+											}
+											?>
+										</select>
+									</div>
+		            			<?php } 
+							}?>
+                    		
+							<div class="form-group">
+								<input type="text" name='rv_name' required class="form-control"  placeholder="Enter name *"> 
+								<input type="hidden" name='rv_post_id' required value="<?php echo get_the_ID(); ?>" >
+                    		</div>  
+							<div class="form-group">
+                       			<input type="text" name='rv_phone' class="form-control" required  placeholder="Enter telephone number (not displayed) *">
+                    		</div>
+							<div class="form-group">
+                       			<input type="email" name='rv_email' class="form-control" required  placeholder="Enter e-mail (not displayed) *">
+                    		</div>
+                   			<div class="form-group"  style="display:none">
+                      			<div class="radio">
+                        			<label>
+                          				<input type="radio"  name="rv_gender" id="optionsRadios1" value="Male" checked>
+                          				Male
+                        			</label>
+                      			</div>
+                      			<div class="radio">
+                        			<label>
+                          				<input type="radio" name="rv_gender" id="optionsRadios2" value="Female">
+                          				Female
+									</label>
+                      			</div>
+                    		</div>
+							<div class="form-group">
+                  				<input type="text" name="review_rating" required class="kv-uni-star rating-loading" value="" data-size="lg" title="">
+        						<br>
+                    		</div>
+							<div class="form-group">
+                      			<textarea class="form-control" rows="4" name="rv_comment" required placeholder="Leave your review...*"></textarea>
+                    		</div>
+                  		</div><!-- /.box-body -->
+                  		<div class="box-footer">
+                    		<button type="submit" name="rv_submit" class="btn btn-primary">Submit Review</button>
+                  		</div>
+                	</form>
+				</div><!-- /.box -->
+			<?php } ?>
 		</div>
-		</div>
+	</div>
+</div>
+
 <script>
     jQuery( document ).ready( function( $ ) {
         $('.kv-gly-star').rating({
@@ -235,8 +231,8 @@
             containerClass: 'is-heart'
         });
         $('.rating,.kv-gly-star,.kv-gly-heart,.kv-uni-star,.kv-uni-rook,.kv-fa,.kv-fa-heart,.kv-svg,.kv-svg-heart').on(
-                'change', function () {
-                    console.log('Rating selected: ' + $(this).val());
-                });
+			'change', function () {
+				console.log('Rating selected: ' + $(this).val());
+		});
     });
 </script>
